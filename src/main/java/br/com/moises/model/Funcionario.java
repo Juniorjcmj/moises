6d52d34efbe4afcae6611e4aa85564595687e060
Juniorjcmj <jotacfs2010@hotmail.com>
2018-09-15 17:12:39 -0300

    Crud pronto

--- src/main/java/br/com/moises/model/Funcionario.java
+++ src/main/java/br/com/moises/model/Funcionario.java
@@ -25,82 +25,74 @@ public class Funcionario implements Serializable {
 	@NotNull(message="CPF é obrigatório")
 	private String cpf;
 	
+	
+	
+	private String situacao;
+	
+	
+	
 	public Funcionario() {
 		// TODO Auto-generated constructor stub
 	}
 
+
+
 	public Funcionario(Integer id, @NotNull(message = "Nome é obrigatório") String nome,
-			@NotNull(message = "CPF é obrigatório") String cpf) {
+			@NotNull(message = "CPF é obrigatório") String cpf, String situacao) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
+		this.situacao = situacao;
 	}
+
+
 
 	public Integer getId() {
 		return id;
 	}
 
+
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+
 
 	public String getNome() {
 		return nome;
 	}
 
+
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
 
 	public String getCpf() {
 		return cpf;
 	}
 
+
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
+
+
+	public String getSituacao() {
+		return situacao;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Funcionario other = (Funcionario) obj;
-		if (cpf == null) {
-			if (other.cpf != null)
-				return false;
-		} else if (!cpf.equals(other.cpf))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		return true;
+
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
 	}
 
-	@Override
-	public String toString() {
-		return "Funcionario [id=" + id + ", nome=" + nome + ", cpf=" + cpf + "]";
-	}
-
+	
 }
